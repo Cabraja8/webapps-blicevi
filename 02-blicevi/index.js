468871383bccc679a0d06ed188f4f6af5e9d4cfe
Ivan Čabraja <ivancabraja0@gmail.com>
2022-11-22 08:58:36 +0100

    rjesen drugi zad

--- 02-blicevi/index.js
+++ 02-blicevi/index.js
@@ -7,7 +7,7 @@ var port = 3000;
 
 app.post("/saveItem", async (req, res) => {
   let db = await connect();
-  let data = await db.collection("saveItem").insertOne({
+  let data = await db.collection("zad1").insertOne({
     data: {
       item: {
         name: "Hlace",
@@ -16,7 +16,23 @@ app.post("/saveItem", async (req, res) => {
       },
     },
   });
-  res.send("Data received:" + JSON.stringify(data));
+  res.send("item hlace saved:" + JSON.stringify(data));
+  res.send("OK");
+});
+
+app.get("/getBrand", async (req, res) => {
+  let cursor = await db.collection("zad1").find({}).sort({});
+  let results = await cursor.toArray();
+
+  res.json(results);
+});
+app.get("/getBrand?brand=New", async (req, res) => {
+  let db = await connect();
+
+  let cursor = await db.collection("zad1").find({ brand: "New" }).sort({});
+  let results = await cursor.toArray();
+
+  res.json(results);
 });
 
 app.listen(port, () => {
